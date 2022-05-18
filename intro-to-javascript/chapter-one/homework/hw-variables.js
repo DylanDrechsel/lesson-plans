492930fe6d5d04dd8d29b58ed9205c9c53e2bdd7
Dylan Drechsel <drechseldylan@gmail.com>
2022-05-18 18:28:09 -0400

    creating homework

---
+++ intro-to-javascript/chapter-one/homework/hw-variables.js
@@ -0,0 +1,34 @@
+// Homework for Variables
+// DONT BE AFRAID TO REFERENCE variables.js
+// Look for patterns and find out what needs to be replaced
+// You can also copy and paste from the other file and just changes the variables
+
+// Console Logs
+// Example
+// write a line that console logs the result from 12 - 5
+console.log(12 - 5) // --> Returns 7
+
+// write a line that console logs the result from 5 + 5
+
+
+// write a line that console logs the result from 8 * 2
+
+
+// write a line that console logs the result from 5 / 5
+
+
+// write a line that console logs the wether 6 is even (0) or odd (1)
+// remember the special divide operator (%) will only return 0 or 1
+
+
+// write a line that console logs the wether 101 is even (0) or odd (1)
+
+
+// Variables
+// Example
+
+
+// Arrays
+// Example
+//    Array Name     Array Value
+const classMembers = ['Dylan', 'Chase', 'Matta', 'Joe']
